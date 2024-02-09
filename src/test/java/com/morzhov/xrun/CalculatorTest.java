968a1488f7c040687a6e0cff81790312051abc44
Stepik <ykuznetsov.dev@gmail.com>
2024-02-08 17:22:32 -0800

    Clean

--- src/test/java/com/morzhov/xrun/CalculatorTest.java
+++ src/test/java/com/morzhov/xrun/CalculatorTest.java
@@ -1,7 +1,6 @@
 package com.morzhov.xrun;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
--- src/test/java/com/morzhov/xrun/DemoApplicationTests.java
+++ src/test/java/com/morzhov/xrun/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.morzhov.xrun;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
